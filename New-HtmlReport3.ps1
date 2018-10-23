5e8b231db8d42984cc24120d544c5d899cd65f59
Anthony Roud <anthoy.roud@amorgroup.local>
2018-10-23 21:00:57 +0100

    initial commit

---
+++ New-HtmlReport3.ps1
@@ -0,0 +1,45 @@
+Set-Location 'C:\Users\anthony.roud\OneDrive\Documents\PowerShell\PSUG\SuperMeetup'
+
+#get processes to work with
+$processes = Get-Process
+
+#Build HTML header
+$HTML = New-HTMLHead -title "Process details" #-cssPath 'stylesheet.css'
+
+#Add CPU time section with top 10 PrivateMemorySize processes.  This example does not highlight any particular cells
+$HTML += "<h3>Process Private Memory Size</h3>"
+$HTML += New-HTMLTable -inputObject $($processes | Sort-Object PrivateMemorySize -Descending | Select-Object name, PrivateMemorySize -first 10)
+
+#Add Handles section with top 10 Handle usage.
+$handleHTML = New-HTMLTable -inputObject $($processes | Sort-Object handles -descending | Select-Object Name, Handles -first 10)
+
+#build hash table with parameters for Add-HTMLTableColor.  Argument and AttrValue will be modified each time we run this.
+$params = @{
+    Column = "Handles" #I'm looking for cells in the Handles column
+    ScriptBlock = {[double]$args[0] -gt [double]$args[1]} #I want to highlight if the cell (args 0) is greater than the argument parameter (arg 1)
+    Attr = "Style" #This is the default, don't need to actually specify it here
+}
+
+#Add yellow, orange and red shading
+$handleHTML = Add-HTMLTableColor -HTML $handleHTML -Argument 1500 -attrValue "background-color:#FFFF99;" @params
+$handleHTML = Add-HTMLTableColor -HTML $handleHTML -Argument 2000 -attrValue "background-color:#FFCC66;" @params
+$handleHTML = Add-HTMLTableColor -HTML $handleHTML -Argument 3000 -attrValue "background-color:#FFCC99;" @params
+
+#Add title and table
+$HTML += "<h3>Process Handles</h3>"
+$HTML += $handleHTML
+
+#gather 20 events from the system log and pick out a few properties
+$events = (Get-WinEvent -LogName System -MaxEvents 200).Where({$level = 'Warning','Error'; $_.LevelDisplayName -in $level}) |
+    Select-Object TimeCreated, ID, LevelDisplayName, ProviderName, Message -First 10
+
+#Create the HTML table without alternating rows, colorize Warning and Error messages, highlighting the whole row.
+$eventTable = $events | New-HTMLTable -setAlternating $false |
+    Add-HTMLTableColor -Argument "Error" -Column "LevelDisplayName" -AttrValue "background-color:#ffb7a3;" -WholeRow
+
+$HTML += "<h3>System Warning Events</h3>"
+$HTML += $eventTable | Close-HTML
+
+# Add content to file and open in browser
+Set-Content .\report_3.htm $HTML
+Invoke-Item .\report_3.htm
